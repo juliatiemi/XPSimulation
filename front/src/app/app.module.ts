0927c137c18c443e5dbcbb0f5763fdb33f17a4b8
Tiemi <mrshttm@gmail.com>
2019-04-10 16:16:00 -0300

    Create project base

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AskComponent } from './ask/ask.component';
+import { QuestionComponent } from './question/question.component';
+import { QuestionListComponent } from './question-list/question-list.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AskComponent
+    AskComponent,
+    QuestionComponent,
+    QuestionListComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
