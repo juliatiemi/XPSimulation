c1174e77c3e5d19b944bdcf506718d303fae077c
Tiemi <mrshttm@gmail.com>
2019-04-10 16:05:19 -0300

    first commit

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AskComponent } from './ask/ask.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
