8451e6b159031c00bd653988b9c6689ecb6eeec1
Tiemi <mrshttm@gmail.com>
2019-04-10 16:26:16 -0300

    Add routing paths

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AskComponent } from './ask/ask.component'
+import { LoginComponent } from './login/login.component'
+import { ProfileComponent } from './profile/profile.component'
+import { QuestionComponent } from './question/question.component'
+import { QuestionListComponent } from './question-list/question-list.component'
+import { RegisterComponent } from './register/register.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ask', component: AskComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'me', component: ProfileComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'questions', component: QuestionListComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
