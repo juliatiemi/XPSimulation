4b218e7550e96dafe2bbb461fd99bd2dde79ca4f
Tiemi <mrshttm@gmail.com>
2019-04-29 01:23:58 -0300

    Make ask question component

--- molotov/front/src/app/ask.service.ts
+++ molotov/front/src/app/ask.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AskService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  submit(body:any){
+    return this._http.post('http://127.0.0.1:3000/ask/submit', body, {
+      observe: 'body',
+      headers: new HttpHeaders().append('Content-Type', 'application/json')
+    });
+  }
 }
