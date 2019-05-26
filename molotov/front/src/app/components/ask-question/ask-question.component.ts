b276cd5233776957cbb3cc1b08fb5babd939acfe
matheus666matheus <matheus.bh2@hotmail.com>
2019-05-26 15:39:28 -0300

    ask-question session

--- molotov/front/src/app/components/ask-question/ask-question.component.ts
+++ molotov/front/src/app/components/ask-question/ask-question.component.ts
@@ -1,27 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionComponent } from '../question/question.component';
 import {Router} from "@angular/router";
+import Axios from 'axios';
 import { AppSettingsService } from 'src/app/app-settings.service';
+
+const baseurl = 'http://localhost:3000/'
+const headers = {
+ 'Content-Type' : 'application/json',
+ 'Access-Control-Allow-Origin': '*'
+}
 
 @Component({
   selector: 'app-ask-question',
   templateUrl: './ask-question.component.html',
   styleUrls: ['./ask-question.component.scss']
 })
+
 export class AskQuestionComponent implements OnInit {
 
   constructor(private router: Router,
     public settings: AppSettingsService) { }
 
   question = {
-    title: "",
     body: ""
   }
 
   //todo
   //send to api
   submit() {
-    
+    var quest = {
+      text: this.question.body,
+      user: localStorage.getItem('username'),
+      points: 0,
+      tags: []
+    }
+
+
+    Axios.post(baseurl + 'questions/ask', quest ,{headers : headers})
+    .then((resp)=> {
+        console.log('aqui')
+        console.log(resp)
+      })
+    .catch((error) => {
+        console.log(error)
+    })
   }
 
   cancel() {
@@ -32,7 +54,7 @@ export class AskQuestionComponent implements OnInit {
   //todo
   //check if it is edit or write
   ngOnInit() {
-    if(!localStorage.getItem(this.settings.LOCALSTORAGE_USERDATA)) {
+    if(!localStorage.getItem('username')) {
       this.router.navigateByUrl('/login');
     }
   }
